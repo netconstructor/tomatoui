07153461e2a3e48fa385a9808df20c65781e8f22
Toni Parviainen <tparvi@gmail.com>
2011-09-11 13:02:19 +0300

    Shared functionality for monthly and daily charts is now in base class

---
+++ BandwidthDownloaderUi/Views/ChartViewModel.cs
@@ -0,0 +1,146 @@
+﻿namespace BandwidthDownloaderUi.Views
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    using BandwidthDownloaderUi.Infra;
+
+    /// <summary>
+    /// Base class for daily and monthly viewmodels.
+    /// </summary>
+    public class ChartViewModel<TValueType, TFilterType> : ViewModel
+    {
+        protected List<TValueType> allValues = new List<TValueType>();
+
+        private List<TValueType> filteredValues = new List<TValueType>();
+
+        public bool FilteringDisabled { get;  protected set; }
+
+        private DateTime? lastUpdated;
+
+        private TransferUnit transferUnit = TransferUnit.Gigabytes;
+
+        private List<TFilterType> filters = new List<TFilterType>();
+
+        private TFilterType filterStart;
+
+        private TFilterType filterEnd;
+
+        public string XValues { get; set; }
+
+        /// <summary>
+        /// Gets or sets FilteredValues.
+        /// </summary>
+        public List<TValueType> FilteredValues
+        {
+            get
+            {
+                return this.filteredValues;
+            }
+
+            set
+            {
+                this.filteredValues = value;
+                this.OnPropertyChanged("FilteredValues");
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets TransferUnit.
+        /// </summary>
+        public TransferUnit TransferUnit
+        {
+            get
+            {
+                return this.transferUnit;
+            }
+
+            set
+            {
+                this.transferUnit = value;
+                this.UpdateAllAndFilteredValues();
+                this.OnPropertyChanged("TransferUnit");
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets Filters.
+        /// </summary>
+        public List<TFilterType> Filters
+        {
+            get
+            {
+                return this.filters;
+            }
+
+            set
+            {
+                this.filters = value;
+                this.OnPropertyChanged("Filters");
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the start filter.
+        /// </summary>
+        public TFilterType FilterStart
+        {
+            get
+            {
+                return this.filterStart;
+            }
+
+            set
+            {
+                this.filterStart = value;
+                this.FilterValues();
+                this.OnPropertyChanged("FilterStart");
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the end filter.
+        /// </summary>
+        public TFilterType FilterEnd
+        {
+            get
+            {
+                return this.filterEnd;
+            }
+
+            set
+            {
+                this.filterEnd = value;
+                this.FilterValues();
+                this.OnPropertyChanged("FilterEnd");
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets LastUpdated.
+        /// </summary>
+        public DateTime? LastUpdated
+        {
+            get
+            {
+                return this.lastUpdated;
+            }
+
+            set
+            {
+                this.lastUpdated = value;
+                this.OnPropertyChanged("LastUpdated");
+            }
+        }
+
+        protected virtual void UpdateAllAndFilteredValues()
+        {            
+        }
+
+        protected virtual void FilterValues()
+        {            
+        }
+    }
+}
--- BandwidthDownloaderUi/Views/MonthlyViewModel.cs
+++ BandwidthDownloaderUi/Views/MonthlyViewModel.cs
@@ -11,24 +11,8 @@
     /// <summary>
     /// View model for the daily view.
     /// </summary>
-    public class MonthlyViewModel : ViewModel
+    public class MonthlyViewModel : ChartViewModel<MonthlyValue, Filter>
     {
-        private List<MonthlyValue> allValues = new List<MonthlyValue>();
-
-        private List<MonthlyValue> filteredValues = new List<MonthlyValue>();
-
-        private List<Filter> filters = new List<Filter>();
-
-        private TransferUnit transferUnit = TransferUnit.Gigabytes;
-
-        private Filter filterStart;
-
-        private Filter filterEnd;
-
-        private bool filteringDisabled;
-
-        private DateTime? lastUpdated;
-
         /// <summary>
         /// Initializes a new instance of the <see cref="MonthlyViewModel"/> class.
         /// </summary>
@@ -38,127 +22,12 @@
         public MonthlyViewModel(IDownloadReportCommand downloadReportCommand)
         {
             downloadReportCommand.ReportDownloaded += this.DownloadCompleted;
+            this.XValues = "Months";
         }
 
-        /// <summary>
-        /// Gets or sets TransferUnit.
-        /// </summary>
-        public TransferUnit TransferUnit
+        protected override void FilterValues()
         {
-            get
-            {
-                return this.transferUnit;
-            }
-
-            set
-            {
-                this.transferUnit = value;
-                this.UpdateAllAndFilteredValues();
-                this.OnPropertyChanged("TransferUnit");
-            }
-        }
-
-        /// <summary>
-        /// Gets FilteredValues.
-        /// </summary>
-        public List<MonthlyValue> FilteredValues
-        {
-            get
-            {
-                return this.filteredValues;
-            }
-
-            private set
-            {
-                this.filteredValues = value;
-                this.OnPropertyChanged("FilteredValues");
-            }
-        }
-
-        /// <summary>
-        /// Gets Filters.
-        /// </summary>
-        public List<Filter> Filters
-        {
-            get
-            {
-                return this.filters;
-            }
-
-            private set
-            {
-                this.filters = value;
-                this.OnPropertyChanged("Filters");
-            }
-        }
-
-        /// <summary>
-        /// Gets or sets the start filter.
-        /// </summary>
-        public Filter FilterStart
-        {
-            get
-            {
-                return this.filterStart;
-            }
-
-            set
-            {
-                this.filterStart = value;
-                this.FilterValues();
-                this.OnPropertyChanged("FilterStart");
-            }
-        }
-
-        /// <summary>
-        /// Gets or sets the end filter.
-        /// </summary>
-        public Filter FilterEnd
-        {
-            get
-            {
-                return this.filterEnd;
-            }
-
-            set
-            {
-                this.filterEnd = value;
-                this.FilterValues();
-                this.OnPropertyChanged("FilterEnd");
-            }
-        }
-
-        /// <summary>
-        /// Gets LastUpdated.
-        /// </summary>
-        public DateTime? LastUpdated
-        {
-            get
-            {
-                return this.lastUpdated;
-            }
-
-            private set
-            {
-                this.lastUpdated = value;
-                this.OnPropertyChanged("LastUpdated");
-            }
-        }
-
-        /// <summary>
-        /// Gets XValues.
-        /// </summary>
-        public string XValues
-        {
-            get
-            {
-                return "Months";
-            }
-        }
-
-        private void FilterValues()
-        {
-            if (this.filteringDisabled)
+            if (this.FilteringDisabled)
             {
                 return;
             }
@@ -167,12 +36,12 @@
             
             if (null != this.FilterStart)
             {
-                result = result.Where(x => (x.Month >= this.filterStart.Month && x.Year >= this.filterStart.Year) || x.Year > this.filterStart.Year);
+                result = result.Where(x => (x.Month >= this.FilterStart.Month && x.Year >= this.FilterStart.Year) || x.Year > this.FilterStart.Year);
             }
 
             if (null != this.FilterEnd)
             {
-                result = result.Where(x => (x.Month <= this.filterEnd.Month && x.Year <= this.filterEnd.Year) || x.Year < this.filterEnd.Year);
+                result = result.Where(x => (x.Month <= this.FilterEnd.Month && x.Year <= this.FilterEnd.Year) || x.Year < this.FilterEnd.Year);
             }
 
             // If we would use observable collection and update status of single values
@@ -188,18 +57,18 @@
 
             // Update with the currently selected transfer unit
             var temp = new List<MonthlyValue>(e.Monthly);            
-            temp.ForEach(x => x.ChangeTransferUnit(this.transferUnit));
+            temp.ForEach(x => x.ChangeTransferUnit(this.TransferUnit));
             this.allValues = temp;
 
             this.Filters = e.Monthly.Select(x => new Filter(x)).ToList();
 
             // Disable the filtering so that setting FilterStart/End
-            // does not cause filtering.);
-            this.filteringDisabled = true;
+            // does not cause filtering.
+            this.FilteringDisabled = true;
 
             if (this.allValues.HasElements())
             {
-                if (null == this.filterStart)
+                if (null == this.FilterStart)
                 {
                     this.FilterStart = new Filter(e.Monthly.FirstOrDefault());
                 }
@@ -210,12 +79,12 @@
                 }                
             }
 
-            this.filteringDisabled = false;
+            this.FilteringDisabled = false;
 
             this.FilterValues();
         }
 
-        private void UpdateAllAndFilteredValues()
+        protected override void UpdateAllAndFilteredValues()
         {
             // This method is a workaround for the fact that I cannot bind to ConverterParameter
             // and WPF Toolkit's chart DependentValueBinding does not support multivalue
@@ -223,13 +92,13 @@
             // whenever the selected transfer unit (KB, MB, GB) changes.      
       
             // Updating all the values so that they are in sync.
-            this.allValues.ForEach(x => x.ChangeTransferUnit(this.transferUnit));
+            this.allValues.ForEach(x => x.ChangeTransferUnit(this.TransferUnit));
 
             // If we would use observable collection and update status of single values
             // inside it then screen would flicker because each change causes it to redraw
             // the scale (Y axis).  We also need to create completely new instance of the list
             // otherwise the chart won't update.            
-            this.FilteredValues = new List<MonthlyValue>(this.filteredValues);
+            this.FilteredValues = new List<MonthlyValue>(this.FilteredValues);
         }
     }
 }
